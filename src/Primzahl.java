976062176212fa5088807ec886b7d8a55c1d8b24
Dr. Dillinger <peter.dillinger@srh.de>
2022-11-16 10:15:14 +0100

    Initial commit

---
+++ src/Primzahl.java
@@ -0,0 +1,9 @@
+public class Primzahl {
+    public static void main(String[] args) {
+        int zahl = 37;
+
+        // TODO
+
+        System.out.println(zahl + " ist eine Primzahl");
+    }
+}
