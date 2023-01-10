5afb245ba6da82c46ab216ab35722e269f86ef96
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-01-10 14:58:57 +0100

    Lauffähige Version...

--- src/Primzahl.java
+++ src/Primzahl.java
@@ -1,9 +1,15 @@
 public class Primzahl {
     public static void main(String[] args) {
-        int zahl = 37;
+        int zahl = 1739;
 
-        // TODO
+        for (int teiler = 2; teiler < zahl; teiler++) {
+            if (zahl % teiler == 0) {
+                System.out.println("Teiler gefunden: " + teiler);
+                System.out.println("KEINE Primzahl!");
+                System.exit(0);
+            }
+        }
 
-        System.out.println(zahl + " ist eine Primzahl");
+        System.out.println(zahl + " ist eine Primzahl!!!");
     }
 }
